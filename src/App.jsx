8829458aa8e09a1efe07e54e0f54b6b8156eb3b8
victoriaponce <micaelaponce199622@gmail.com>
2026-08-09 18:09:19 -0300

    agregamos el maquetado del header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import Header from "./components/Header";
 
 function App() {
-  
+  //AQUI VA LA LÓGICA DEL COMPONENTE
   return (
-    <h1>Hola Mundo </h1>
-  )
+    <>
+    {/*AQUI COMIENZ<A LA ZONA DEL HTML}
+      {/*<Header></Header>*/}
+      <Header/>
+      <h1>Hola Mundo </h1>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header className="container my-4">
+      <h1 className="display-6">Práctica de react</h1>
+      <p>
+        React es una biblioteca de JavaScript desarrollada pór Facebook para
+        construir interfaces de usuario. Se destaca por su enfoque en la
+        creacion de aplicaciones web de una sola página SPA y su capacidad para
+        manejar la interfaz de manera eficiente mediante el uso de un DOM
+        vistual <br /> React permite a los desarrolladores construir interfaces
+        dinámicas y reactivas de forma modular , loque facilita el desaroollo y
+        el manteniemiento de aplicaciones a gran escala
+      </p>
+    </header>
+  );
+};
+
+export default Header;
