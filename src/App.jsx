52eb95dffb1f2a0bdfb6ab68f190c9e7c0a0b308
victoriaponce <micaelaponce199622@gmail.com>
2026-08-09 18:49:32 -0300

    agregamos el componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Header from "./components/Header";
 import Teoria from "./components/Teoria";
+import Footer from "./components/Footer"
+
 function App() {
   //AQUI VA LA LÓGICA DEL COMPONENTE
   return (
     <>
-    {/*AQUI COMIENZ<A LA ZONA DEL HTML}
+      {/*AQUI COMIENZ<A LA ZONA DEL HTML}
       {/*<Header></Header>*/}
-      <Header/>
-     <main className="container">
-      <Teoria></Teoria>
-     </main>
+      <Header />
+      <main className="container">
+        <Teoria></Teoria>
+      </main>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer
+      className="bg-black  text-center py-3
+      
+  "
+    >
+      <p>&copy;Todos los derechos reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
